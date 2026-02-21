0ba235725fb418b3cf1427ad081d3bd2b5ab11c3
kalyanasundaram1901 <kkssanjay1901@gmail.com>
2026-02-21 10:41:29 +0530

    Update AdminDashboard.js

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE_URL = `http://${window.location.hostname}:5000`;
+const API_BASE_URL = `https://medication-assistant.onrender.com`;
 
 const AdminDashboard = () => {
     const [medicines, setMedicines] = useState([]);
